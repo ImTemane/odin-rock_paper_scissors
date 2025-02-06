3c960e3953c6f848168ecc8145e542616de32f80
ImTemane <170125706+ImTemane@users.noreply.github.com>
2025-02-06 11:45:26 +0100

    Add the getHumanChoice function
    - to get the player choice
    - to be able to fight the computer!

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ const attacks = ["rock","paper","scissors"];
 
 function getComputerChoice() {
   return attacks[Math.ceil(Math.random() * 3)]
-} 
+};
+
+function getHumanChoice() {
+  let choice = prompt("Choose your attack: rock, paper or scissors?  ").toLowerCase;
+  if (choice in attacks) {
+    return choice
+  }
+  else {
+    console.log("Invalid Choice: check the spell pls.");
+    getComputerChoice();
+  };
+};
