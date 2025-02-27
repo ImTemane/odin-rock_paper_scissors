dbd7274b60c43cb8d038598b427aee4a7bbb9421
ImTemane <170125706+ImTemane@users.noreply.github.com>
2025-02-27 12:14:39 +0100

    Add CheckGameEnd() and make the code readable
    - Added to end the game when a player has 5 points
    - Made to simplify reading
    
    Enjoy

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ const scoreDisplayer = document.querySelector("p.score");
 const roundWinnerDisplayer = document.querySelector("p.round-winner");
 const gameWinnerDisplayer = document.querySelector("p.game-winner");
 
-rockBtn.addEventListener("click",() => playRound("rock"));
-paperBtn.addEventListener("click",() => playRound("paper"));
-scissorsBtn.addEventListener("click",() => playRound("scissors"));
-
 const attacks = ["rock","paper","scissors"];
 let humanScore = 0;
 let computerScore = 0;
+
+const playRock = () => { playRound("rock");checkGameEnd();};
+const playPaper = () => { playRound("paper");checkGameEnd();};
+const playScissors = () => { playRound("scissors");checkGameEnd();};
+
+rockBtn.addEventListener("click", playRock);
+paperBtn.addEventListener("click",playPaper);
+scissorsBtn.addEventListener("click",playScissors);
 
 function getComputerChoice() {
   return attacks[Math.floor(Math.random() * 3)]
@@ -40,3 +44,15 @@ function playRound(humanChoice, computerChoice = getComputerChoice()) {
   }
   scoreDisplayer.textContent = "You: " + humanScore + " | Computer: " + computerScore;
 };
+
+function checkGameEnd() {
+  if (humanScore === 5 || computerScore === 5) {
+    rockBtn.removeEventListener("click", playRock);
+    paperBtn.removeEventListener("click",playPaper);
+    scissorsBtn.removeEventListener("click",playScissors);
+    gameWinnerDisplayer.textContent = humanScore = 5 ? "You won the game!" : "Computer won the game.";
+    return true
+  } else {
+    return false
+  }
+}
